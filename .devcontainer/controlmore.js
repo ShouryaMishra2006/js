7e75bef442eac02a0eb5c9a9ac5d9da1d73f84f2
Shourya Mishra <150536894+ShouryaMishra2006@users.noreply.github.com>
2024-06-30 14:16:56 +0000

    control more

---
+++ .devcontainer/controlmore.js
@@ -0,0 +1,5 @@
+//NULLISH COALESCING OPERATOR (??):null undefined
+let val1=5??10//val1 is assigned 5
+let val2=null??10//val2 is assigned 10
+let val3=undefined??15//15 is assigned
+let val4=null??2??3//first true value is executed
