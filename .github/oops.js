051a18251a297c6918040defb31b28526eb93241
Shourya Mishra <150536894+ShouryaMishra2006@users.noreply.github.com>
2024-07-11 20:18:57 +0000

    objects

--- .github/oops.js
+++ .github/oops.js
@@ -30,3 +30,48 @@ String.prototype.truelength=function(){
     console.log(this.trim().length)
 }
 str.truelength()
+const con=Object.getOwnPropertyDescriptors(Math,"PI")
+console.log(con)//we can define property and set its attributes as not enumerable not writable not changeable etc etc
+chait={
+    price:250,
+    type:"ginger"
+}
+Object.defineProperty(chait,'type',{
+    writable:false,
+    enumerable:false
+})
+for(let [key,value] of Object.entries(chait)){
+    if(typeof key!=='function'){
+    console.log(`${key}:${value}`)
+    }
+}//only price is iterable and hence it will be printed only
+class usere{
+    constructor(username,password){
+        this.username=username
+        this.password=password
+    }
+    get password(){
+       return `${this._password}abc`.toUpperCase()
+    }
+    //if get is there then set should also be there with same name as property which we wanna manipulate
+    set password(value){
+       this._password=value//we are using the underscore so that max call stack can not be exceeded
+    }
+}
+//the above method of using getter and setter is more preffered as due to this personally we can customize the usecase of each properties of the class
+const shourya=new usere("mishra","hehe")
+console.log(shourya.password)
+function useree(username,password){
+    this._username=username
+    this._password=password
+    Object.defineProperty(this,'username',{
+        get:function(){
+            return this._email.toUpperCase()
+        },
+        set:function(value){
+            this._email=value
+        }
+    })
+}
+const meee=new useree("shourya","mishra")
+console.log(meee._password)
